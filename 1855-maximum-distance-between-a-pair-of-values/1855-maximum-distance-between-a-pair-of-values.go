ad40d8792a77aca6480abbebf13eec18ab84114f
Hyungsub  Kim <aa_aa_aa_11@naver.com>
2026-02-01 10:59:40 +0900

    Time: 0 ms (100%), Space: 11.2 MB (52.94%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.go
@@ -0,0 +1,14 @@
+func maxDistance(nums1 []int, nums2 []int) int {
+    i := 0
+    j := 0
+    ans := 0
+    for i < len(nums1) && j < len(nums2){
+        if nums1[i] <= nums2[j] {
+            ans = max(ans, j-i)
+            j++
+        } else {
+            i++
+        }
+    }
+    return ans
+}
